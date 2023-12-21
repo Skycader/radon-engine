d74e8fb5545afe84a8706157e5249ab8f191157d
axlmemote <daniil.ustimencko@gmail.com>
2023-12-21 17:22:04 +0300

    main: work awesome

--- src/app/components/project-block/project-block.component.ts
+++ src/app/components/project-block/project-block.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../../models/config.interface';
 
 @Component({
   selector: 'app-project-block',
   templateUrl: './project-block.component.html',
-  styleUrl: './project-block.component.scss'
+  styleUrl: './project-block.component.scss',
 })
 export class ProjectBlockComponent {
-
+  @Input() project!: Project;
 }
--- src/app/components/projects-list/projects-list.component.ts
+++ src/app/components/projects-list/projects-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../../models/config.interface';
 
 @Component({
   selector: 'app-projects-list',
   templateUrl: './projects-list.component.html',
-  styleUrl: './projects-list.component.scss'
+  styleUrl: './projects-list.component.scss',
 })
 export class ProjectsListComponent {
-
+  @Input() projects: Project[] = [];
 }
