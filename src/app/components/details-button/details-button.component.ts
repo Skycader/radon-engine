c192d6f970c0d2e677c0d3d3c98d1d37b9c9610f
skycader <skycader@yandex.ru>
2023-12-22 17:01:31 +0300

    fillment

--- src/app/components/details-button/details-button.component.ts
+++ src/app/components/details-button/details-button.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-details-button',
   templateUrl: './details-button.component.html',
-  styleUrl: './details-button.component.scss'
+  styleUrl: './details-button.component.scss',
 })
 export class DetailsButtonComponent {
-
+  @Input() detailsUrl!: string;
 }
